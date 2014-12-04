0ec0b6b1abd4e80cda806da105ac680e79eb35d8
Brandon Harrison <brandon@philadelphiagamelab.org>
2014-12-04 00:44:45 -0500

    Added 1st checkpoint to the scene. Added layer Checkpoint so the camera doesn't render the checkpoint mesh

--- UnityProject/Assets/Scripts/KillPlayer.cs
+++ UnityProject/Assets/Scripts/KillPlayer.cs
@@ -5,13 +5,17 @@ public class KillPlayer : MonoBehaviour
 {
 	public GameObject checkpointMgr;
 
+	void Start() {
+		checkpointMgr = GameObject.FindGameObjectWithTag ("CheckpointManager");
+	}
+
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Player") {
 			if (checkpointMgr != null)
-					checkpointMgr.GetComponent<CheckpointMgrScript> ().Spawn ();
+				checkpointMgr.GetComponent<CheckpointMgrScript> ().Spawn ();
 			else
-					Application.LoadLevel(Application.loadedLevel);
+				Application.LoadLevel(Application.loadedLevel);
 		} else if(other.gameObject.tag == "PowerCell") {
 			other.gameObject.GetComponent<Powercell>().ReturnToStartPosition();
 		}
